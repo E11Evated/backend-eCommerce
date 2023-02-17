023fbae388c6bfdff516214959444f02b28b8319
gorillaRouge <john.schoppmann@yahoo.com>
2023-02-17 16:51:10 -0700

    added more code trying commands

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const routes = require('./routes');
+require('dotenv').config();
 // import sequelize connection
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3306;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
+
+// log database credentials
+console.log(process.env.DB_NAME);
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASSWORD);
+console.log(process.env.DB_HOST);
+console.log(process.env.DB_PORT);
 
 // sync sequelize models to the database, then turn on the server
 app.listen(PORT, () => {
