02647899c25eb99f460ee38b711680360240ff5a
gorillaRouge <john.schoppmann@yahoo.com>
2023-02-17 17:30:47 -0700

    add code

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 // import sequelize connection
 
 const app = express();
-const PORT = process.env.PORT || 3306;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
